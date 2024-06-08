9f31e2dc1ea10caab61ba4b8b2039645b0f459d9
yo-zh <sasha7onoff@gmail.com>
2024-06-08 11:26:58 +0300

    Expose volume to be UI-adjustable

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -16,6 +17,7 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Slider effectsSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Button soundBack;
+    [SerializeField] private AudioMixer audioMixer;
 
     [Header("Difficulty")]
     [SerializeField] private Toggle easyToggle;
@@ -25,11 +27,30 @@ public class MainMenu : MonoBehaviour
 
     void Start()
     {
-        
+        startButton.onClick.AddListener(StartGame);
+        effectsSlider.onValueChanged.AddListener(delegate { AdjustVolume(effectsSlider, effectsSlider.value); });
+        musicSlider.onValueChanged.AddListener(delegate { AdjustVolume(musicSlider, musicSlider.value); });
     }
 
     void Update()
     {
         
+    }
+
+    private void StartGame()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    private void AdjustVolume(Slider changedSlider, float value)
+    {
+        if (changedSlider.name == effectsSlider.name)
+        {
+            audioMixer.SetFloat("EffectsVolume", value);
+        }
+        else if (changedSlider.name == musicSlider.name)
+        {
+            audioMixer.SetFloat("MusicVolume", value);
+        }
     }
 }
